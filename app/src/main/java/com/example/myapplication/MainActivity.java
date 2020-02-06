b367db6378b6d032881c6ede933f1c424cd876b7
Skye Hyndman <shyndman@ualberta.ca>
2020-02-05 22:43:47 -0700

    added new classes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -21,18 +21,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Market m = new Market(0.9f, 0.6f, 0.8f);
-        Button button = (Button) findViewById(R.id.next1);
+        Button button = (Button) findViewById(R.id.select_admin);
+        Button button1 = (Button) findViewById(R.id.select_player);
         button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+               //put admin auth stuff
+               Intent intent = new Intent(getBaseContext(), Manager_Instructions.class);
+        //intent.putExtra("PLAYER_ID", name);
+        //startActivity(intent);
+               }});
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText id = (EditText) findViewById(R.id.name);
-                String name = id.getText().toString();
-                FirebaseDatabase fb = FirebaseDatabase.getInstance();
-                fb.getReference().child("players").child(name).setValue(new Player(name));
-                Intent intent = new Intent(getBaseContext(), Manager_Instructions.class);
-                intent.putExtra("PLAYER_ID", name);
-                startActivity(intent);
-
+                //put stuff for player auth
             }
         });
 
